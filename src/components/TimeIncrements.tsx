da730068f4bc4128e6eb62b4dc35a87eb09326f8
SeanMee86 <seanmee8186@gmail.com>
2024-09-24 21:01:37 -0700

    add check for max time to prevent callstack error

--- src/components/TimeIncrements.tsx
+++ src/components/TimeIncrements.tsx
@@ -10,22 +10,22 @@ const TimeIncrements: React.FunctionComponent<ITimeIncrementsProps> = ({
   totalTime,
 }) => (
   <div className="absolute bottom-0 w-full h-full flex">
-    {totalTime !== 0 &&
+    {totalTime <= 10000 &&
+      totalTime !== 0 &&
       timeIncrements !== 0 &&
       Array.from(Array(Math.floor(totalTime / timeIncrements)).keys()).map(
         (n) => (
-          <>
-            <div
-              className="border-r-2 border-[rgb(20,20,20)]/10 h-full relative"
-              style={{
-                width: `${(timeIncrements / totalTime) * 100}%`,
-              }}
-            >
-              <div className="absolute bottom-0 right-0 translate-x-[50%] translate-y-[120%]">
-                {timeIncrements * (n + 1)}ms
-              </div>
+          <div
+            key={n}
+            className="border-r-2 border-[rgb(20,20,20)]/10 h-full relative"
+            style={{
+              width: `${(timeIncrements / totalTime) * 100}%`,
+            }}
+          >
+            <div className="absolute bottom-0 right-0 translate-x-[50%] translate-y-[120%]">
+              {timeIncrements * (n + 1)}ms
             </div>
-          </>
+          </div>
         )
       )}
   </div>
